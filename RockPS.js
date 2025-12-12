95c6ccc15e14ba8127c9d6fbec2167b52ab627cc
miloszgad <miloszgad@onet.pl>
2025-12-12 20:57:11 +0100

    MISTAKE IN INDEX.HTML AND MECHANISM IN JAVASCRIPT
    In that actualization I have fixed "source" on line 7 of index and also I was working on mechanism of playing round in my game.
    I have added playRound funnction and have made some changes at auto computer choices (getcomputerChoice function).

--- RockPS.js
+++ RockPS.js
@@ -1,1 +1,61 @@
 console.log("hello world")
+
+function getComputerChoice() {
+   const n = Math.floor(Math.random() * 3);
+if (n === 0) return "rock";
+if (n === 1) return "paper";
+if (n === 2) return "scissors";
+}
+
+
+function getHumanChoice() { 
+    return prompt("Rock, Paper or Scissors?");
+
+}
+
+let computerScore = 0
+let humanScore = 0
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if ( 
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "rock"
+        ) 
+           { humanScore++;
+             alert("You won!");
+  }  else if ( 
+        humanChoice === "scissors" && computerChoice === "rock" ||
+        humanChoice === "paper" && computerChoice === "scissors" ||
+        humanChoice === "rock" && computerChoice === "paper"
+        ) 
+        {computerScore++;
+             alert("You lost.");
+   } else if (
+        humanChoice === computerChoice     
+        )
+            { alert("A draw.")
+                return; 
+    } else {
+        alert("Incorrect word")
+    }
+         
+    }
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+     
+    function playGame() {
+        
+        
+}
+
+
+    
+
+
+
